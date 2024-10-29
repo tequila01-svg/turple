d64549cc17465112ddb5ea0fe1b276ee0b007d4d
tequila01-svg <onifadeayobami008@gmail.com>
2024-10-29 17:54:10 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#empty tuple
+my_tuple=()
+print(my_tuple)
+#number tuple
+my_tuple=(1,2,3,4,5)
+print(my_tuple)
+#mixed tuple
+my_tuple=('yam', 'beans', 1,2,3)
+print(my_tuple)
+print(my_tuple)
+print(my_tuple[2])
+#slicing
+print(my_tuple[2:4])
